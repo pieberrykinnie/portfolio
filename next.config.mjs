aee9bfba27eaf9ebd04293729204cea7b260fb8d
pieberrykinnie <dmtruong1980@gmail.com>
2024-12-24 13:37:58 -0600

    fix: Fix config for both dev and deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-        output: 'export',
-        basePath: '/portfolio',
-        images: {
-                path: "/",
-        },
-};
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
 
-export default nextConfig;
+export default (phase) => {
+        const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+        /** @type {import('next').NextConfig} */
+
+        const nextConfig = {
+                output: isDev ? undefined : 'export',
+                basePath: isDev ? undefined : '/portfolio',
+                assetPrefix: isDev ? undefined : 'https://pieberrykinnie.github.io/portfolio/',
+        };
+        return nextConfig;
+}
