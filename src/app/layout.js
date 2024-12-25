a2c5088b521f7e5fd32a4260738446d5f63b3ffa
pieberrykinnie <dmtruong1980@gmail.com>
2024-12-25 13:43:12 -0600

    fix: Vercel plugins for real

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
-import Analytics from "@vercel/analytics";
-import SpeedInsights from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
