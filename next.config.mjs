c2ecb082a78d37304231fd2706e8f9c6ff061d2f
pieberrykinnie <dmtruong1980@gmail.com>
2024-12-24 20:45:51 -0600

    chore: Try empty config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
 
-export default (phase) => {
+const NextConfig = (phase) => {
         const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
         /** @type {import('next').NextConfig} */
 
         const nextConfig = {
-                output: isDev ? undefined : 'export',
-                basePath: isDev ? undefined : '/portfolio',
-                assetPrefix: isDev ? undefined : 'https://pieberrykinnie.github.io/portfolio/',
+                // output: isDev ? undefined : 'export',
+                // basePath: isDev ? undefined : '/portfolio',
+                // assetPrefix: isDev ? undefined : 'https://pieberrykinnie.github.io/portfolio/',
         };
+
         return nextConfig;
 }
+
+export default NextConfig;
